8d647714de9462be3c2bb458e5115d9c538d53a7
srianeesh328 <66383558+srianeesh328@users.noreply.github.com>
2024-08-17 14:21:51 -0400

    Update TeamInviteRepository.java

--- src/main/java/com/reveture/project2/repository/TeamInviteRepository.java
+++ src/main/java/com/reveture/project2/repository/TeamInviteRepository.java
@@ -1,16 +1,16 @@
 package com.reveture.project2.repository;
 
-import com.reveture.project2.entities.Team;
-import com.reveture.project2.entities.Team;
+import com.reveture.project2.entities.TeamInvite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface TeamRepository extends JpaRepository<Team, UUID> {
+public interface TeamInviteRepository extends JpaRepository<TeamInvite, UUID> {
 
-
+    Optional<TeamInvite> findBySenderManager_Team_TeamId(UUID teamId);
 
 
 }
