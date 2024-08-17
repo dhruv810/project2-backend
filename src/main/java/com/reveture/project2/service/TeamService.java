ccc7f0f932f1fda0f7b287efdf5dba2bb8e6213f
srianeesh328 <66383558+srianeesh328@users.noreply.github.com>
2024-08-17 14:17:37 -0400

    Update TeamService.java

--- src/main/java/com/reveture/project2/service/TeamService.java
+++ src/main/java/com/reveture/project2/service/TeamService.java
@@ -1,7 +1,25 @@
 package com.reveture.project2.service;
 
+import com.reveture.project2.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.reveture.project2.entities.*;
+
+import java.util.UUID;
 
 @Service
 public class TeamService {
+
+
+    @Autowired
+    final private TeamRepository teamRepository;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+
+
+
+
 }
