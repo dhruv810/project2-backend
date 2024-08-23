c808df5e3276cfe99819904c05d3c90511371c28
dhruv810 <dhruvfadadu11@gmail.com>
2024-08-23 07:14:20 -0700

    Added proposalID in TeamProposalDTO

--- src/main/java/com/reveture/project2/DTO/TeamProposalDTO.java
+++ src/main/java/com/reveture/project2/DTO/TeamProposalDTO.java
@@ -3,15 +3,19 @@ package com.reveture.project2.DTO;
 import com.reveture.project2.entities.TeamProposal;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class TeamProposalDTO {
 
+    private UUID proposalId;
     private Double amount;
     private String status;
     private String sponsor_name;
     private String team_name;
 
     public TeamProposalDTO(TeamProposal teamProposal) {
+        this.proposalId = teamProposal.getProposalId();
         this.amount = teamProposal.getAmount();
         this.team_name = teamProposal.getReceiverTeam().getTeamName();
         this.status = teamProposal.getStatus();
