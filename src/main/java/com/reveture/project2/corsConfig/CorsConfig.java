6c30246b519de59dd520d1b8f25e56db5b317e3a
dhruv810 <dhruvfadadu11@gmail.com>
2024-08-27 18:59:12 -0700

    hosting on ec2

--- src/main/java/com/reveture/project2/corsConfig/CorsConfig.java
+++ src/main/java/com/reveture/project2/corsConfig/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // React frontend URL
+                        .allowedOrigins("**") // React frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowCredentials(true);
             }
